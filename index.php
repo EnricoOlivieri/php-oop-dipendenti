32ad3e1a036f9fc2aa1e0c0d3190c067a7d432dc
Enrico Olivieri <enrico.olivieri@live.it>
2021-01-18 16:31:00 +0100

    class person in

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+class Persona
+{
+    protected $nome;
+    protected $cognome;
+    protected $codice_fiscale;
+
+    public function __construct($nome, $cognome, $codice_fiscale) {
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->codice_fiscale = $codice_fiscale;
+    }
+
+    public function to_string() {
+        foreach ($this as $key => $value) {
+            print "$key : $value";
+            print "<br>";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+?>
